fbd40b595b7912c6bd6460227714a9da71862b8e
adrianorabello <adrianorabello@icloud.com>
2025-12-13 12:59:42 +0000

    v1.1

--- Trabalho-final/project/state.tf
+++ Trabalho-final/project/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mba-fiap-trabalho-final"
-    key    = "teste"
+    key    = "project/${terraform.workspace}/terraform.tfstate"
     region = "us-east-1"
   }
 }
